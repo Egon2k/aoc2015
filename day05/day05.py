fbd6813ee02e79cf0f9ad95f1e75c123dde81717
Rene Trampenau <rtrampenau@gmail.com>
2021-11-30 21:31:33 +0100

    solved day05

--- day05/day05.py
+++ day05/day05.py
@@ -1,33 +1,51 @@
-import hashlib
-
-def checkVovels(line):
+def checkPart1Rule1(line):
     count = 0
     for vovel in ['a','e','i','o','u']:
-        if vovel in line:
-            count = count + 1
+        count = count + line.count(vovel)
     return (count >= 3)
 
-def checkDoubles(line):
+def checkPart1Rule2(line):
     for i, char in enumerate(line[:-1]):
         if line[i] == line[i+1]:
             return True
     return False
 
-def checkBadStrings(line):
-    return "ab" in line or "cd" in line or "pq" in line or "xy" in line
+def checkPart1Rule3(line):
+    return not("ab" in line or "cd" in line or "pq" in line or "xy" in line)
+
+
+def checkPart2Rule1(line):
+    for i, char in enumerate(line[:-2]):
+        if line[i:i+2] in line[i+2:]:
+            return True
+    return False
+
+def checkPart2Rule2(line):
+    for i, char in enumerate(line[:-2]):
+        if line[i] == line[i+2]:
+            return True
+    return False
+
+def checkNicePart1(line):
+    return checkPart1Rule1(line) and checkPart1Rule2(line) and checkPart1Rule3(line)
+
+def checkNicePart2(line):
+    return checkPart2Rule1(line) and checkPart2Rule2(line)
 
 def part1(data):
     count = 0
     for i in data:
-        if checkVovels(i) and checkDoubles(i) and not(checkBadStrings(i)):
+        if checkNicePart1(i):
             count = count + 1
-        
     return count
-        
 
 def part2(data):
-    pass
-        
+    count = 0
+    for i in data:
+        if checkNicePart2(i):
+            count = count + 1
+    return count
+
 if __name__ == "__main__":
     data = []
 
@@ -36,4 +54,4 @@ if __name__ == "__main__":
             data.append(line.strip())
 
     print(part1(data))
-    #print(part2(data))
+    print(part2(data))
